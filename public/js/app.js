645282ff6e96d14cad3912d110af8b694576e7bc
Chau23445 <135419773+Chau23445@users.noreply.github.com>
2023-06-03 23:06:13 +0700

    Add

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+// const image = document.querySelector(".img-lastt");
+// let i = 0;
+// let timeID = setTimeout( function xoay(){
+//     i = i + 5
+//     image.style.transform = "rotate("+i+"deg)";
+//     timeID = setTimeout(xoay, 100);
+// }, 1000)
